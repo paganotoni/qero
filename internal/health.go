2f6a90c7f68d0c0b2bb924b02a07656ff0e16191
Antonio Pagano <a@pagano.id>
2025-10-28 10:03:19 -0500

    adding missing comment

--- internal/health.go
+++ internal/health.go
@@ -2,6 +2,8 @@ package internal
 
 import "net/http"
 
+// A simple healthcheck required by Kamal, since this app does not have any
+// dependency that needs to be checked it just returns OK.
 func health(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("OK"))
 }
